ee007a586ddc9350c2d0fe3130f1697cd8e1163d
Анастасия Пакаева <staroxy63@gmail.com>
2022-05-29 19:02:41 +0300

    fixed #1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,15 @@
 public class Main {
-  public static void main(String[] args) {
-    BonusService service = new BonusService();
+    public static void main(String[] args) {
+        BonusService service = new BonusService();
 
-    long amount = 1000_60;
-    boolean registered = true;
+        long amount = 1000_60;
+        boolean registered = true;
 
-    service.calculate(amount, registered);
-  }
+        long expected = 30; // ожидаемый результат
+        long actual = service.calculate(amount, registered); //вызов метода
+        boolean passed = expected == actual; //сравнение
+
+        System.out.println(passed);
+    }
 }
 
